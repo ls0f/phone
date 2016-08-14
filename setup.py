6457d4b6850d17bd3f4ab0e839259450d28cf86a
lovedboy <lovedboy.tk@qq.com>
2016-08-14 11:53:39 +0800

    modify version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DESCRIPTION = "手机号码库"
 AUTHOR = "lovedboy"
 AUTHOR_EMAIL = "admin@lovedboy.com"
 URL = "https://github.com/lovedboy/phone"
-VERSION = '0.3.2'
+VERSION = '0.4.0'
 
 setup(
     name=NAME,
